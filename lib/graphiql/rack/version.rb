e618858102c08c99060622a80c495512911b5501
Matthew Banbury <mbanbury@gmail.com>
2022-06-10 18:10:36 -0600

    Init

---
+++ lib/graphiql/rack/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Graphiql
+  module Rack
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/graphiql/rack_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Graphiql::Rack do
+  it "has a version number" do
+    expect(Graphiql::Rack::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
